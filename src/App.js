c5f0749978b3f5766b82d47703b416ae41c70de0
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-26 10:49:47 +0800

    add StudentList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import Counter from './ex.1-1025/Counter'
-import JSXValue from './ex.2-1026-1/JSXValue'
+// import JSXValue from './ex.2-1026-1/JSXValue'
+import StudentList from './ex.3-1026-2/StudentList'
 
 function App() {
   return (
     <>
-      <JSXValue />
+      <StudentList />
     </>
   )
 }
---
+++ src/ex.3-1026-2/StudentList.js
@@ -0,0 +1,9 @@
+function StudentList() {
+  return (
+    <>
+      <h1>學生名單</h1>
+    </>
+  )
+}
+
+export default StudentList
