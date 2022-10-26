df24bd8c7a021c81a269c551c86008c7d03ec78e
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-26 09:38:47 +0800

    expression

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Counter from './ex.1-1025/Counter'
+// import Counter from './ex.1-1025/Counter'
+import JSXValue from './ex.2-1026-1/JSXValue'
 
 function App() {
   return (
     <>
-      <Counter />
+      <JSXValue />
     </>
   )
 }
---
+++ src/ex.2-1026-1/JSXValue.js
@@ -0,0 +1,7 @@
+function JSXValue() {
+  return (<>
+    <h1>JSX中的值與表達式</h1>
+  </>)
+}
+
+export default JSXValue
