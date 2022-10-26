6704b3fdf8e0443502a371516d41e03e446889be
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-26 09:57:07 +0800

    different types of value/expression

--- src/ex.2-1026-1/JSXValue.js
+++ src/ex.2-1026-1/JSXValue.js
@@ -1,7 +1,25 @@
 function JSXValue() {
-  return (<>
-    <h1>JSX中的值與表達式</h1>
-  </>)
+  return (
+    <>
+      <h1>JSX中的值與表達式</h1>
+      <hr />
+      <h3>Number</h3>
+      {123}
+      {NaN}
+      <h3>String</h3>
+      {'abc'}
+      {`total = ${100 - 45}`}
+      <h3>Boolean</h3>
+      {true}
+      {false}
+      <h3>Object</h3>
+      {/* {{a: 1, b: 2}} */}
+      <h3>Array</h3>
+      {[1, 2, 3, 4]}
+      <h3>Function</h3>
+      {(() => 1)()}
+    </>
+  )
 }
 
 export default JSXValue
