11757a1f9013b7f8f5528904b90bd193b714c2b7
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-28 11:23:17 +0800

    add radio group (via map)

--- src/ex.10-1028-2/HTML5Form.js
+++ src/ex.10-1028-2/HTML5Form.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react'
 
 function HTML5Form() {
   const [inputText, setInputText] = useState('')
+  const [textAreaText, setTextAreaText] = useState('')
+
+  const [agree, setAgree] = useState(false)
+
+  const [gender, setGender] = useState('')
+  const genderOPtions = ['male', 'female', 'none']
 
   return (
     <>
@@ -16,11 +22,38 @@ function HTML5Form() {
       <hr />
       <h1>文字輸入區域(textarea)</h1>
       <textarea
-        value={inputText}
+        value={textAreaText}
         onChange={(e) => {
-          setInputText(e.target.value)
+          setTextAreaText(e.target.value)
         }}
       />
+      <hr />
+      <h1>核取方塊(checkbox single)</h1>
+      <input
+        type="checkbox"
+        checked={agree}
+        onChange={(e) => {
+          setAgree(e.target.checked)
+        }}
+      />
+      <label>同意會員註冊條款</label>
+      <hr />
+      <h1>選項按鈕群組(radio grouop)</h1>
+      {genderOPtions.map((v, i) => {
+        return (
+          <div key={i}>
+            <input
+              type="radio"
+              checked={gender === v}
+              value={v}
+              onChange={(e) => {
+                setGender(e.target.value)
+              }}
+            />
+            <label>{v}</label>
+          </div>
+        )
+      })}
     </>
   )
 }
