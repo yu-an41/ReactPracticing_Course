6acbb1fe70436464096a4086c47fc9d920bad135
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-10-31 10:58:04 +0800

    add onChange

--- src/ex.12-1031-2/BirthSelect.js
+++ src/ex.12-1031-2/BirthSelect.js
@@ -1,4 +1,13 @@
+import { useState } from 'react'
+
 function BirthSelect() {
+  const [year, setYear] = useState('')
+  const [month, setMonth] = useState('')
+  const [day, setDay] = useState('')
+
+  // 製作下拉選單項目使用的函式
+  // params: min(:number), max(:number)
+  // return number[]
   const makeOptions = (min, max) => {
     const options = []
 
@@ -12,7 +21,13 @@ function BirthSelect() {
     <>
       <span>
         西元
-        <select>
+        <select
+          value={year}
+          onChange={(e) => {
+            setYear(e.target.value)
+          }}
+        >
+          <option value="">請選擇</option>
           {makeOptions(1920, 2010).map((v, i) => {
             return (
               <option key={i} value={v}>
@@ -20,11 +35,26 @@ function BirthSelect() {
               </option>
             )
           })}
+          {/* {Array(2010 - 1920 + 1)
+            .fill(1)
+            .map((v, i) => {
+              return (
+                <option key={i} value={i + 1920}>
+                  {i + 1920}
+                </option>
+              )
+            })} */}
         </select>
         年
       </span>
       <span>
-        <select>
+        <select
+          value={month}
+          onChange={(e) => {
+            setMonth(e.target.value)
+          }}
+        >
+          <option value="">請選擇</option>
           {makeOptions(1, 12).map((v, i) => {
             return (
               <option key={i} value={v}>
@@ -36,7 +66,13 @@ function BirthSelect() {
         月
       </span>
       <span>
-        <select>
+        <select
+          value={day}
+          onChange={(e) => {
+            setDay(e.target.value)
+          }}
+        >
+          <option value="">請選擇</option>
           {makeOptions(1, 29).map((v, i) => {
             return (
               <option key={i} value={v}>
