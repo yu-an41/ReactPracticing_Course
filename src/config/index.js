de036bca80be14b20d8f24911ef5c7642785bb22
Yvonne Cheng <yvonne840401tw@gmail.com>
2022-11-03 09:49:44 +0800

    import devUrl via config/.env

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+export const appConfig = {
+  debug: true,
+  devUrl: 'http://localhost:3000',
+  prodUrl: 'http://www.abc.com',
+}
+// 開發期間(debug: true) 用devUrl存取圖片
+// 完成後(debug: false) 用prodUrl存取
+
+export const imgUrl = appConfig.debug ? appConfig.devUrl : appConfig.prodUrl
--- src/ex.18-1103-1/ImageDemo.js
+++ src/ex.18-1103-1/ImageDemo.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import LogoImage from './logo.svg'
 
+import { imgUrl } from '../config'
+
 function ImageDemo() {
   return (
     <>
       <h1>圖片使用說明</h1>
       <hr />
       <p>1. 直接用`import`放在src目錄</p>
-      <img src={LogoImage} alt="" />
+      <img src={LogoImage} alt="" width="200" height="200" />
+      <p>2. 放在public目錄，用絕對網址存取</p>
+      <img
+        //  套用config/index.js 中的設定值
+        src={`${imgUrl}/Images/Screenshot%202022-10-23%20at%2011.30.16%20AM.png`}
+        alt=""
+        height="200"
+      />
+      <img
+        // 套用 .env中的設定值
+        src={`${process.env.REACT_APP_DEV_URL}/Images/Screenshot%202022-10-23%20at%2011.30.16%20AM.png`}
+        alt=""
+        height="200"
+      />
     </>
   )
 }
